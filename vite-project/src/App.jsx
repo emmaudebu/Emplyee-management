3da7d80249541547ea6bd883773ae07f2e7cdb2f
ifeanyi chukwu <emmanueludebu@gmail.com>
2024-12-25 17:57:34 +0100

    login page created

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Login from "./Components/Login";
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Login />
     </>
   );
 }
--- vite-project/src/Components/Login.jsx
+++ vite-project/src/Components/Login.jsx
@@ -1,43 +1,52 @@
 import React from "react";
+import "./style.css";
 
 const Login = () => {
-  return;
-  <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
-    <div className=" p-3 rounded w-25 border loginForm">
-      <h2>Login page</h2>
-      <form>
-        <div>
-          <label htmlFor="email">
-            {" "}
-            <strong>Email</strong>
-          </label>
-          <input
-            type="email"
-            name="email"
-            id=""
-            autoComplete="off"
-            placeholder="Enter email"
-            className="form-control rounded-0"
-          />
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
+        <div className=" p-3 rounded w-25 border loginForm">
+          <h2>Login page</h2>
+          <form>
+            <div className=" mb-3">
+              <label htmlFor="email">
+                {" "}
+                <strong>Email</strong>
+              </label>
+              <input
+                type="email"
+                name="email"
+                id=""
+                autoComplete="off"
+                placeholder="Enter email"
+                className="form-control rounded-0"
+              />
+            </div>
+            <div className=" mb-3">
+              <label htmlFor="password">
+                {" "}
+                <strong>password</strong>
+              </label>
+              <input
+                type="password"
+                name="password"
+                id=""
+                placeholder="Enter password"
+                className="form-control rounded-0"
+              />
+            </div>
+            <button className="btn btn-success w-100 rounded-0 mb-2">
+              Login
+            </button>
+            <div className=" mb-1">
+              <input type="checkbox" name="tick" className="me-2" />
+              <label htmlFor="tick"> You agree with term and conditions</label>
+            </div>
+          </form>
         </div>
-        <div>
-          <label htmlFor="password">
-            {" "}
-            <strong>password</strong>
-          </label>
-          <input
-            type="password"
-            name="password"
-            id=""
-            placeholder="Enter password"
-            className="form-control rounded-0"
-          />
-        </div>
-        <button className="btn btn-success w-100 rounded-0">Login</button>
-      </form>
-      ;
-    </div>
-  </div>;
+      </div>
+    </>
+  );
 };
 
 export default Login;
